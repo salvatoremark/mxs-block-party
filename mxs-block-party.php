c3fc3412bf1b7771faea878757704b1bc951ceae
Mark Salvatore <salvatore.mark@gmail.com>
2024-05-10 17:10:43 -0400

    put blocks into mxs category

--- mxs-block-party.php
+++ mxs-block-party.php
@@ -32,9 +32,15 @@ if (!class_exists('Msalv_MxsBlockParty')) {
      * @see https://developer.wordpress.org/reference/functions/register_block_type/
      */
     function block_init() {
+      add_filter('block_categories_all', array($this, 'create_block_category'));
       register_block_type(__DIR__ . '/build/text-shadower');
       register_block_type(__DIR__ . '/build/text-slicer');
       register_block_type(__DIR__ . '/build/golden-text');
+    }
+    function create_block_category($categories) {
+      array_unshift($categories, ['slug' => 'mxs', 'title' => 'MXS BLOCKS']);
+      // wp_send_json($categories);
+      return $categories;
     }
   }
   $msalvMxsBlockParty = new Msalv_MxsBlockParty();
